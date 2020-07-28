536d19fd572c901cc36b2916ddaae17de84b6fd5
Sabil <sabilmdtv1@gmail.com>
2020-07-28 19:03:29 +0100

    COnfirmed, Deaths and Recovered fetching and adding on the page

--- app.js
+++ app.js
@@ -13,6 +13,30 @@ const API_URL = "https://api.covid19api.com/summary";
 
 async function covid() {
   const res = await fetch(API_URL);
-  console.log(res);
+  const data = await res.json();
+  console.log(data);
+
+  if (res.status === 4 || res.status === 200) {
+    date.textContent = new Date(data.Date).toDateString();
+    const {
+      TotalConfirmed,
+      TotalDeaths,
+      TotalRecovered,
+      NewConfirmed,
+      NewDeaths,
+      NewRecovered,
+    } = data.Global;
+    // Total Confirmed
+    confirmed.children[1].textContent = TotalConfirmed;
+    confirmed.children[2].textContent = NewConfirmed;
+    // Total Deaths
+    deaths.children[1].textContent = TotalDeaths;
+    deaths.children[2].textContent = NewDeaths;
+    // Total Recovered
+    recovered.children[1].textContent = TotalRecovered;
+    recovered.children[2].textContent = NewRecovered;
+  } else {
+    chart.innerHTML = `<h2>Loading...<.h2>`;
+  }
 }
 covid();
